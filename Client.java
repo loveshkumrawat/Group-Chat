86381ad940d55b4a8df6e3882b286a59d86f1114
Lovesh Kumrawat <lovesh.kumrawat@47billion.com>
2023-01-12 03:30:15 +0530

    Single client Chat

--- Client.java
+++ Client.java
@@ -2,32 +2,92 @@ import java.io.*;
 import java.net.*;
 
 class Client {
+	
+	int delay = 100;
+	Boolean exit = false;
+	Thread recvThread, sendThread;
+	String senderName, recvText = "", consoleTxt, endRule = "exit";
 
-	public static void main(String args[]) throws Exception {
+	public void user() throws Exception {
 
 		// Client socket
-		Socket client = new Socket("localhost", 5080);
+		Socket client;
+		
+		System.out.println("Wait session starting...");
+		while (true) {
+			try {
+				client = new Socket("localhost", 3333);
+				break;
+			} catch (Exception e) {}
+		}
+
+		// Console Reader
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
 		// Data exchange Objects
-		DataOutputStream send = new DataOutputStream(client.getOutputStream());			// to send data to the server
-		BufferedReader recv = new BufferedReader(new InputStreamReader(client.getInputStream()));			// to read data coming from the server
-		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));			// to read data from the keyboard
+		PrintStream send = new PrintStream(client.getOutputStream());
+		BufferedReader recv = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+		// User details exchange
+		senderName = recv.readLine();
+		System.out.println("Connected with " + senderName);
+		System.out.print("Enter your name: ");
+		consoleTxt = console.readLine();
+		System.out.println();
+		send.println(consoleTxt);
 		
-		String consoleTxt, recvTxt;
-		
-		while (true) {
-			if ((consoleTxt = console.readLine()).equals("exit")) break;
-			send.writeBytes(consoleTxt + "\n");
-			if ((recvTxt = recv.readLine()).equals("exit")) break;
-			System.out.println(recvTxt);
+		while (!exit) {
+
+			// Minimum delay for proper execution
+			Thread.sleep(delay);
+			
+			// Reciever port
+			if (recvThread == null || !recvThread.isAlive()) {
+				
+				recvThread = new Thread() {
+					public void run() {
+						
+						try {
+							if ((recvText = recv.readLine()).equals(endRule)) exit = true;
+							System.out.println(senderName + ": " + recvText);
+						} catch (Exception e) {
+							System.out.println("Disconnected: Running Thread...");
+							exit = true;
+						}
+					}
+				};
+
+				recvThread.start();
+			}
+			
+			// Sender port
+			if (sendThread == null || !sendThread.isAlive()) {
+				
+				sendThread = new Thread() {
+					public void run() {
+						
+						try {
+							if ((consoleTxt = console.readLine()).equals(endRule)) exit = true;
+							if (!recvText.equals(endRule)) send.println(consoleTxt);
+						} catch (Exception e) {
+							System.out.println("Disconnected: Running Thread...");
+							exit = true;
+						}
+					}
+				};
+
+				sendThread.start();
+			}
 		}
 		
-		send.writeBytes("exit");
-
-		// close connection.
+		// Disconnection
 		send.close();
 		recv.close();
 		console.close();
 		client.close();
+	}
+
+	public static void main(String args[]) throws Exception {
+		new Client().user();
 	}
 }
--- Server.java
+++ Server.java
@@ -2,38 +2,90 @@ import java.io.*;
 import java.net.*;
 
 class Server {
+	
+	int delay = 100;
+	Boolean exit = false;
+	Thread recvThread, sendThread;
+	String senderName, recvText = "", consoleTxt, endRule = "exit";
 
-	public static void main(String args[]) throws Exception {
+	public void user() throws Exception {
 
 		// Server Socket
-		ServerSocket server = new ServerSocket(5080);
+		ServerSocket server = new ServerSocket(3333);
+
+		// Console Reader
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
 		// Connection to Client Socket
+		System.out.print("Enter your name: ");
+		consoleTxt = console.readLine();
 		System.out.println("Waiting for client...");
 		Socket client = server.accept();
-		System.out.println("Connection established");
-
+		
 		// Data exchange Objects
-		PrintStream send = new PrintStream(client.getOutputStream());			// to send data to the client
-		BufferedReader recv = new BufferedReader(new InputStreamReader(client.getInputStream()));			// to read data coming from the client
-		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));			// to read data from the keyboard
-		
-		String recvText, consoleTxt;
-		
-		while (true) {
-			if ((recvText = recv.readLine()).equals("exit")) break;
-			System.out.println(recvText);
-			if ((consoleTxt = console.readLine()).equals("exit")) break;
-			send.println(consoleTxt);
-		}
+		PrintStream send = new PrintStream(client.getOutputStream());
+		BufferedReader recv = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-		send.println("exit");
+		// User details exchange
+		send.println(consoleTxt);
+		senderName = recv.readLine();
+		System.out.println("Connected with " + senderName + "\n");
 		
-		// close connection
+		while (!exit) {
+
+			// Minimum delay for proper execution
+			Thread.sleep(delay);
+			
+			// Reciever port
+			if (recvThread == null || !recvThread.isAlive()) {
+				
+				recvThread = new Thread() {
+					public void run() {
+						
+						try {
+							if ((recvText = recv.readLine()).equals(endRule)) exit = true;
+							System.out.println(senderName + ": " + recvText);
+						} catch (Exception e) {
+							System.out.println("Disconnected: Running Thread...");
+							exit = true;
+						}
+					}
+				};
+
+				recvThread.start();
+			}
+			
+			// Sender port
+			if (sendThread == null || !sendThread.isAlive()) {
+				
+				sendThread = new Thread() {
+					public void run() {
+						
+						try {
+							if ((consoleTxt = console.readLine()).equals(endRule)) exit = true;
+							if (!recvText.equals(endRule)) send.println(consoleTxt);
+						} catch (Exception e) {
+							System.out.println("Disconnected: Running Thread...");
+							exit = true;
+						}
+					}
+				};
+
+				sendThread.start();
+			}
+		}
+		
+		// Disconnection
 		send.close();
 		recv.close();
 		console.close();
-		server.close();
 		client.close();
+		
+		// Server shut-down
+		server.close();
+	}
+
+	public static void main(String args[]) throws Exception {
+		new Server().user();
 	}
 }
